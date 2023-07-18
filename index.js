f4b212f33da31fa08b5b985b0257b3d2882c19de
Raman-Lamba <ramanlamba2022@gmail.com>
2023-07-18 17:21:06 +0530

    js local storage try

--- index.js
+++ index.js
@@ -3,68 +3,94 @@ const tasks = document.querySelector(".tasks");
 const clearAll = document.querySelector(".clear");
 const messageSpan = document.querySelector(".message span");
 const searchForm = document.querySelector(".search");
+let taskList = [];
 
-function updateMessage(){
-    const textLength = tasks.children.length;
-    messageSpan.textContent = `You have ${textLength} pending tasks.`;
-}
-updateMessage();
-addForm.addEventListener("submit",event =>{
-    event.preventDefault();
-    const value = addForm.task.value.trim();
-
-    if(value.length){
-        tasks.innerHTML += ` <li>
-                                <span>${value}</span>
-                                <i class="bi bi-trash-fill delete"></i>
-                            </li>`;
-        addForm.reset();
-        updateMessage();
-    }
-});
-
-tasks.addEventListener("click",event =>{
-    if(event.target.classList.contains("delete")){
-        event.target.parentElement.remove();
-        updateMessage();
-    }
-});
-
-clearAll.addEventListener("click",event =>{
-      const taskItems = tasks.querySelectorAll("li");
-      taskItems.forEach(item => {
-        item.remove();
-      });
-      updateMessage();
-});
-
-function filterTask(term){
-    const list =  Array.from(tasks.children)
-    .filter(task =>{
-        return !task.textContent.toLowerCase().includes(term);
-    })
-    .forEach(task =>{
-        task.classList.add("hide");  
+// Load tasks from storage
+function loadTasks() {
+  if (localStorage.getItem("tasks")) {
+    taskList = JSON.parse(localStorage.getItem("tasks"));
+    taskList.forEach(task => {
+      tasks.innerHTML += `<li>
+                            <span>${task}</span>
+                            <i class="bi bi-trash-fill delete"></i>
+                          </li>`;
     });
-    Array.from(tasks.children)
-    .filter(task=>{
-        return task.textContent.toLowerCase().includes(term);
-    })
-    .forEach(task => {
-        task.classList.remove("hide");
-    });
-
+    updateMessage();
+  }
 }
 
-searchForm.addEventListener("keyup", event =>{
-    const term = searchForm.task.value.trim().toLowerCase();
+// Save tasks to storage
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+}
+
+function updateMessage() {
+  const textLength = taskList.length;
+  messageSpan.textContent = `You have ${textLength} pending tasks.`;
+}
+
+// Add task
+addForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const value = addForm.task.value.trim();
+
+  if (value.length) {
+    taskList.push(value);
+    tasks.innerHTML += `<li>
+                          <span>${value}</span>
+                          <i class="bi bi-trash-fill delete"></i>
+                        </li>`;
+    addForm.reset();
+    updateMessage();
+    saveTasks();
+  }
+});
+
+// Delete task
+tasks.addEventListener("click", event => {
+  if (event.target.classList.contains("delete")) {
+    const taskItem = event.target.parentElement;
+    const taskIndex = Array.from(tasks.children).indexOf(taskItem);
+    taskItem.remove();
+    taskList.splice(taskIndex, 1);
+    updateMessage();
+    saveTasks();
+  }
+});
+
+// Clear all tasks
+clearAll.addEventListener("click", event => {
+  tasks.innerHTML = "";
+  taskList = [];
+  updateMessage();
+  saveTasks();
+});
+
+// Filter tasks
+function filterTask(term) {
+  Array.from(tasks.children).forEach(task => {
+    const taskText = task.querySelector("span").textContent.toLowerCase();
+    if (taskText.includes(term)) {
+      task.classList.remove("hide");
+    } else {
+      task.classList.add("hide");
+    }
+  });
+}
+
+// Search tasks
+searchForm.addEventListener("keyup", event => {
+  const term = searchForm.task.value.trim().toLowerCase();
+  filterTask(term);
+});
+
+searchForm.addEventListener("click", event => {
+  if (event.target.classList.contains("reset")) {
+    searchForm.reset();
+    const term = searchForm.task.value.trim();
     filterTask(term);
+  }
 });
 
-searchForm.addEventListener("click", event =>{
-    if(event.target.classList.contains("reset")){
-        searchForm.reset();
-        const term = searchForm.task.value.trim();
-        filterTask(term);
-    }
-});
+// Load tasks on page load
+window.addEventListener("load", loadTasks);
